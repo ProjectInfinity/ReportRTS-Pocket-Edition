fea33d9e4f3d7d5fcde619a657d5f5e0ccf6c348
ProjectInfinity <a.sjur91@gmail.com>
2014-09-08 15:33:41 +0200

    Initial commit.

---
+++ src/ProjectInfinity/ReportRTS/ReportRTS.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace ProjectInfinity\ReportRTS;
+
+use pocketmine\plugin\PluginBase;
+
+class ReportRTS extends PluginBase {
+
+    public $ticketMax;
+    public $ticketDelay;
+    public $ticketMinWords;
+    public $ticketPerPage;
+    public $ticketPreventDuplicates;
+    public $ticketNag;
+    public $ticketNagHeld;
+    public $ticketHideOffline;
+
+    public $debug;
+
+    public function onEnable() {
+        $this->getLogger()->info("Welcome to the Alpha for ReportRTS. Please report any bugs you may discover,
+        this project is a large Bukkit project that is being ported to PocketMine.");
+        $this->reloadSettings();
+    }
+
+    public function reloadSettings() {
+        $this->saveDefaultConfig();
+        $this->reloadConfig();
+
+        # Ticket configuration.
+        $this->ticketMax = $this->getConfig()->get("ticket.max");
+        $this->ticketDelay = $this->getConfig()->get("ticket.delay");
+        $this->ticketMinWords = $this->getConfig()->get("ticket.minimumWords");
+        $this->ticketPerPage = $this->getConfig()->get("ticket.perPage");
+        $this->ticketPreventDuplicates = $this->getConfig()->get("ticket.preventDuplicates");
+        $this->ticketNag = $this->getConfig()->get("ticket.nag");
+        $this->ticketNagHeld = $this->getConfig()->get("ticket.nagHeld");
+        $this->ticketHideOffline= $this->getConfig()->get("ticket.hideOffline");
+
+        # Shows debug information in the plugin if enabled.
+        $this->debug = $this->getConfig()->get("debug");
+    }
+}
