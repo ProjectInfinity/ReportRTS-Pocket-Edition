91a6bbee5615823e30670e8f35ee81c44d4ff07b
ProjectInfinity <a.sjur91@gmail.com>
2014-09-15 16:42:23 +0200

    Update colors array. This should be a function in TextFormat, similarly to ChatColor.values() in Bukkit. It is useful.

--- src/ProjectInfinity/ReportRTS/util/MessageHandler.php
+++ src/ProjectInfinity/ReportRTS/util/MessageHandler.php
@@ -12,12 +12,18 @@ class MessageHandler {
 
     public static function load() {
         MessageHandler::$generalError = '%red%An error occurred. Reference: %s';
-        MessageHandler::$colors = array("%red%" => TextFormat::RED);
-        MessageHandler::$colors = array("%black%" => TextFormat::BLACK);
-        MessageHandler::$colors = array("%dark_blue%" => TextFormat::DARK_BLUE);
-        MessageHandler::$colors = array("%dark_green%" => TextFormat::DARK_GREEN);
-        MessageHandler::$colors = array("%dark_aqua%" => TextFormat::DARK_AQUA);
-        // TODO: Finish above.
+        MessageHandler::$colors = array("%black%" => TextFormat::BLACK,
+            "%dark_blue%" => TextFormat::DARK_BLUE, "%dark_green%" => TextFormat::DARK_GREEN,
+            "%dark_aqua%" => TextFormat::DARK_AQUA, "%dark_red%" => TextFormat::DARK_RED,
+            "%dark_purple%" => TextFormat::DARK_PURPLE, "%gold%" => TextFormat::GOLD,
+            "%gray%" => TextFormat::GRAY, "%dark_gray%" => TextFormat::DARK_GRAY,
+            "%blue%" => TextFormat::BLUE, "%green%" => TextFormat::GREEN,
+            "%aqua%" => TextFormat::AQUA, "%red%" => TextFormat::RED,
+            "%light_purple%" => TextFormat::LIGHT_PURPLE, "%yellow%" => TextFormat::YELLOW,
+            "%white%" => TextFormat::WHITE, "%obfuscated%" => TextFormat::OBFUSCATED,
+            "%bold%" => TextFormat::BOLD, "%strikethrough%" => TextFormat::STRIKETHROUGH,
+            "%underline%" => TextFormat::UNDERLINE, "%italic%" => TextFormat::ITALIC,
+            "%reset%" => TextFormat::RESET);
     }
 
     private function parseColors($message) {
