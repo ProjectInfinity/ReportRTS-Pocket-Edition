d2e1259cce770a17114cda46e7a708f50e555668
ProjectInfinity <a.sjur91@gmail.com>
2014-09-15 20:07:42 +0200

    Reflection is possible.

--- src/ProjectInfinity/ReportRTS/util/MessageHandler.php
+++ src/ProjectInfinity/ReportRTS/util/MessageHandler.php
@@ -12,18 +12,7 @@ class MessageHandler {
 
     public static function load() {
         MessageHandler::$generalError = '%red%An error occurred. Reference: %s';
-        MessageHandler::$colors = array("%black%" => TextFormat::BLACK,
-            "%dark_blue%" => TextFormat::DARK_BLUE, "%dark_green%" => TextFormat::DARK_GREEN,
-            "%dark_aqua%" => TextFormat::DARK_AQUA, "%dark_red%" => TextFormat::DARK_RED,
-            "%dark_purple%" => TextFormat::DARK_PURPLE, "%gold%" => TextFormat::GOLD,
-            "%gray%" => TextFormat::GRAY, "%dark_gray%" => TextFormat::DARK_GRAY,
-            "%blue%" => TextFormat::BLUE, "%green%" => TextFormat::GREEN,
-            "%aqua%" => TextFormat::AQUA, "%red%" => TextFormat::RED,
-            "%light_purple%" => TextFormat::LIGHT_PURPLE, "%yellow%" => TextFormat::YELLOW,
-            "%white%" => TextFormat::WHITE, "%obfuscated%" => TextFormat::OBFUSCATED,
-            "%bold%" => TextFormat::BOLD, "%strikethrough%" => TextFormat::STRIKETHROUGH,
-            "%underline%" => TextFormat::UNDERLINE, "%italic%" => TextFormat::ITALIC,
-            "%reset%" => TextFormat::RESET);
+        MessageHandler::$colors = (new \ReflectionClass(TextFormat::class))->getConstants();
     }
 
     private function parseColors($message) {
