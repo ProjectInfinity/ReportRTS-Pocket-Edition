a02c7ba075a8359ae9768f6c2d69642fd985bba5
ProjectInfinity <a.sjur91@gmail.com>
2014-09-16 14:21:02 +0200

    Add relative time function.

--- src/ProjectInfinity/ReportRTS/util/ToolBox.php
+++ src/ProjectInfinity/ReportRTS/util/ToolBox.php
@@ -2,7 +2,15 @@
 
 namespace ProjectInfinity\ReportRTS\util;
 
+use pocketmine\utils\TextFormat;
+
 class ToolBox {
+
+# Set up constants for relative time function.
+const SECOND_MILLIS = 1000;
+const MINUTE_MILLIS =  60000; # 60 * SECOND_MILLIS;
+const HOUR_MILLIS = 3600000; # 60 * MINUTE_MILLIS;
+const DAY_MILLIS = 86400000; # 24 * HOUR_MILLIS;
 
     /**
      * Returns a cleaned up String from the text provided, preferably from a sign.
@@ -21,6 +29,31 @@ class ToolBox {
         $i = 0;
         
         return $i;
+    }
+
+    public static function timeSince($time) {
+
+        # If time is given in seconds as opposed to milliseconds, convert.
+        if($time < 1000000000000) $time *= 1000;
+
+        $now = microtime(true) * 1000;
+        if($time > $now || $time <= 0) return null;
+        $diff = $now - $time;
+        if($diff < self::MINUTE_MILLIS) {
+            return TextFormat::GREEN . "just now" . TextFormat::GOLD;
+        } else if($diff < 2 * self::MINUTE_MILLIS) {
+            return TextFormat::GREEN . "1 minute ago" . TextFormat::GOLD; // a minute ago
+        } else if($diff < 50 * self::MINUTE_MILLIS) {
+            return "" . TextFormat::GREEN . round($diff / self::MINUTE_MILLIS) . " min ago" . TextFormat::GOLD;
+        } else if($diff < 90 * self::MINUTE_MILLIS) {
+            return TextFormat::GREEN . "1 hour ago" . TextFormat::GOLD;
+        } else if ($diff < 24 * self::HOUR_MILLIS) {
+            return "" . TextFormat::YELLOW . round($diff / self::HOUR_MILLIS) . " hours ago" . TextFormat::GOLD;
+        } else if ($diff < 48 * self::HOUR_MILLIS) {
+            return TextFormat::RED . "yesterday" . TextFormat::GOLD;
+        } else {
+            return "" . TextFormat::RED . round($diff / self::DAY_MILLIS) . " days ago" . TextFormat::GOLD;
+        }
     }
 
 }
